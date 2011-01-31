1399261569ce88e390930a9789a3dac32e94e762
Michael Wright <mdwrigh2@ncsu.edu>
2011-01-31 15:05:03 -0500

    Working on the parser

--- pyice_parser.py
+++ pyice_parser.py
@@ -1,4 +1,37 @@
 import yacc
-import pyice-lexer
+import pyice_lexer
 
-tokens = pyice-lexer.tokens
+tokens = pyice_lexer.tokens
+
+#def p_statement(t):
+    #'''stm : if 
+           #| do
+           #| fa
+           #| BREAK SEMI
+           #| EXIT SEMI'''
+def p_statement_02(t):
+    '''stm : RETURN SEMI'''
+
+def p_statement_03(t):
+    '''stm : exp SEMI'''
+
+def p_statement_04(t):
+    '''stm : SEMI'''
+
+def p_expression(t):
+    '''exp : INT'''
+
+#def p_expression_02(t):
+
+
+#def p_expression
+
+
+def p_empty(t):
+    'empty :'
+    pass
+
+def p_error(t):
+    print "Syntax error around line %d" % t.lineno
+
+yacc.yacc(debug=1)
